f274828e197dd65b0738b59786f5c91a918a5362
ThexProdige <maxime.vilette@outlook.fr>
2019-05-09 20:51:16 +0200

    Add files via upload

--- Animation/caca/client.lua
+++ Animation/caca/client.lua
@@ -7,77 +7,44 @@ Citizen.CreateThread(function()
 	end
 end)
 
-function caca()
-            SetCurrentPedWeapon(GetPlayerPed(-1), GetHashKey("WEAPON_UNARMED"), true) --c'est plus réel sans armes ^^
 
-            anim_dico = "missfbi3ig_0"
-            anim_nom = "shit_loop_trev"
-	        RequestAnimDict(anim_dico) --Animation
-	        while not HasAnimDictLoaded(anim_dico) do --tant que Anim pas charger
-	            Citizen.Wait(10)
-	        end 
+function animation(anim_dico,anim_nom,particle_dico,particle_name,px,py,pz,rx,ry,rz,deciBone,taille,lx,ly,lz)
+	SetCurrentPedWeapon(GetPlayerPed(-1), GetHashKey("WEAPON_UNARMED"), true) --c'est plus réel sans armes ^^
 
+	RequestAnimDict(anim_dico) --Animation
+	while not HasAnimDictLoaded(anim_dico) do --tant que Anim pas charger
+	    Citizen.Wait(10)
+	end 
 
-	        local particleDictionary = "scr_amb_chop" --particule 
-	    	local particleName = "ent_anim_dog_poo" --particule
+	RequestNamedPtfxAsset(particle_dico)
 
+	while not HasNamedPtfxAssetLoaded(particle_dico) do --tant que particule pas charger
+		Citizen.Wait(0)
+	end
 
+	local PlayerPed = GetPlayerPed(GetPlayerFromServerId(player))
 
-		    RequestNamedPtfxAsset(particleDictionary)
+	SetPtfxAssetNextCall(particle_dico)
 
-		    while not HasNamedPtfxAssetLoaded(particleDictionary) do --tant que particule pas charger
-		        Citizen.Wait(0)
-		    end
+	local bone = GetPedBoneIndex(PlayerPed, deciBone)
+	local effect
 
-			local PlayerPed = GetPlayerPed(GetPlayerFromServerId(player))
+	TaskPlayAnim(PlayerPedId(), anim_dico, anim_nom, 8.0, -8, -1, 0, 0, 0, 0, 0) --Animation
 
-		    SetPtfxAssetNextCall(particleDictionary)
-
-		    local bone = GetPedBoneIndex(PlayerPed, 11816)
-		    local effect
-
-	        TaskPlayAnim(PlayerPedId(), anim_dico, anim_nom, 8.0, -8, -1, 0, 0, 0, 0, 0) --Animation
-	        effect = StartParticleFxLoopedOnPedBone(particleName, PlayerPed, 0.0, -0.15, -0.1, -140.0, 0.0, 0.0, bone, 1.1, false, false, false) --https://runtime.fivem.net/doc/natives/#_0xF28DA9F38CD1787C
+	effect = StartParticleFxLoopedOnPedBone(particle_name, PlayerPed, px, py, pz, rx, ry, rz, bone, taille, lx, ly, lz) --https://runtime.fivem.net/doc/natives/#_0xF28DA9F38CD1787C
 	        
-	        Wait(6000) --fais lanimation pendant 7s
-	        ClearPedTasks(PlayerPedId())
-            StopParticleFxLooped(effect, 0)
+	Wait(6000) --fais lanimation pendant 7s
+	ClearPedTasks(PlayerPedId())
+    StopParticleFxLooped(effect, 0)
+end
+
+
+function caca()
+	animation("missfbi3ig_0","shit_loop_trev","scr_amb_chop","ent_anim_dog_poo",0.0,0.0,-0.7,0.0,0.0,20.0,11816,1.1,false, false, false)    
 end
 
 function vomit()
-            anim_dico = "anim@amb@business@cfm@cfm_machine_oversee@"
-            anim_nom = "cough_spit_operator"
-            RequestAnimDict(anim_dico) --Animation
-            while not HasAnimDictLoaded(anim_dico) do --tant que Anim pas charger
-                Citizen.Wait(10)
-            end 
-
-
-            --local particleDictionary = "scr_amb_chop" --particule 
-            --local particleName = "ent_anim_dog_poo" --particule
-
-            local particleDictionary = "cut_paletoscore" --particule 
-            local particleName = "cs_paleto_vomit" --particule
-
-            RequestNamedPtfxAsset(particleDictionary)
-
-            while not HasNamedPtfxAssetLoaded(particleDictionary) do --tant que particule pas charger
-                Citizen.Wait(0)
-            end
-
-            local PlayerPed = GetPlayerPed(GetPlayerFromServerId(player))
-
-            SetPtfxAssetNextCall(particleDictionary)
-
-            local bone = GetPedBoneIndex(PlayerPed, 46240)
-            local effect
-
-            TaskPlayAnim(PlayerPedId(), anim_dico, anim_nom, 8.0, -8, -1, 0, 0, 0, 0, 0) --Animation
-            effect = StartParticleFxLoopedOnPedBone(particleName, PlayerPed, 0.0, 0.35, 0.5, -140.0, 0.0, 0.0, bone, 1.1, false, false, false) --https://runtime.fivem.net/doc/natives/#_0xF28DA9F38CD1787C
-            
-            Wait(6000) --fais lanimation pendant 7s
-            ClearPedTasks(PlayerPedId())
-            StopParticleFxLooped(effect, 0)
+	animation("anim@amb@business@cfm@cfm_machine_oversee@","cough_spit_operator","cut_paletoscore","cs_paleto_vomit",0.0,0.35,0.5,-140.0,0.0,0.0,46240,1.1,false, false, false)
 
 end
 
